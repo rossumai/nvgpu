c7956b8850468c047903ccebe996dfc035d3051e
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2018-06-02 16:37:57 +0200

    Increment version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nvgpu',
-      version='0.4',
+      version='0.5',
       description='NVIDIA GPU tools',
       url='https://github.com/rossumai/nvgpu',
       author='Bohumir Zamecnik, Rossum',
