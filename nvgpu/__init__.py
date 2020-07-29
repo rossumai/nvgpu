cbda75035f0be331ff997805ed716ca10c562574
Felix Petersen <mail@felix-petersen.de>
2020-07-29 21:44:07 +0200

    Continue Fix for CUDA 11

--- nvgpu/__init__.py
+++ nvgpu/__init__.py
@@ -12,12 +12,14 @@ def gpu_info():
     lines = _run_cmd(['nvidia-smi'])
     cuda_version = float(lines[2].split('CUDA Version: ')[1].split(' ')[0])
     if cuda_version < 11:
-        selected_lines = lines[7:7 + 3 * gpu_count]
+        line_distance = 3
+        selected_lines = lines[7:7 + line_distance * gpu_count]
     else:
-        selected_lines = lines[8:8 + 4 * gpu_count]
+        line_distance = 4
+        selected_lines = lines[8:8 + line_distance * gpu_count]
     for i in range(gpu_count):
         mem_used, mem_total = [int(m.strip().replace('MiB', '')) for m in
-                               selected_lines[3 * i + 1].split('|')[2].strip().split('/')]
+                               selected_lines[line_distance * i + 1].split('|')[2].strip().split('/')]
         gpu_infos[i]['mem_used'] = mem_used
         gpu_infos[i]['mem_total'] = mem_total
         gpu_infos[i]['mem_used_percent'] = 100. * mem_used / mem_total
